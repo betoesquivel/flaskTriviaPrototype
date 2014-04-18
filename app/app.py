2f223d02e9da783465ea52604f6b290698004413
Beto Esquivel <ppbeto94@gmail.com>
2014-04-18 13:41:31 -0500

    bootstrap and jquery loaded

--- app/app.py
+++ app/app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+from flask.ext.script import Manager
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../static", static_url_path="/static")
+app.debug = True
+
 
 @app.route('/')
 def index():
     return render_template('index.jinja2.html')
 
+manager = Manager(app)
 if __name__ == '__main__':
-    app.run(debug = True)
+    manager.run()
