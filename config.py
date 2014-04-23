d57b95c00808e4045df1dc57f27f615b632bf79e
Beto Esquivel <ppbeto94@gmail.com>
2014-04-22 22:11:33 -0500

    Removed authomatic provider id and hardcoded a short_name myself

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ CONFIG = {
     'fb': {
 
         'class_': oauth2.Facebook,
-        'id': authomatic.provider_id(),
+        'short_name': 1,
         # Facebook is an AuthorizationProvider too.
         'consumer_key': FBAPPKEY,
         'consumer_secret': FBAPPSECRET,
